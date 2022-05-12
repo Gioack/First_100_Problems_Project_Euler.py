0c3fa30b15f82fab1ce63fd9053fb6c29d02511d
Gioack <101208747+Gioack@users.noreply.github.com>
2022-05-12 15:21:33 +0200

    make it readable and fixed a small bug

--- P10_Summation_of_primes.py
+++ P10_Summation_of_primes.py
@@ -1,15 +1,16 @@
-# DO NOT EXECUTE SO HEAVY
 def isprime(num):
     for n in range(2,int(num**0.5)+1):
         if num%n==0:
             return False
     return True
-# DO NOT EXECUTE SO HEAVY
-def Summation_of_primes():
-    Sum = 0
-    for x in range(2000000):
-        if isprime(x):
-            Sum += x
-    return Sum
 
-# DO NOT EXECUTE SO HEAVY 
+
+def summation_of_primes():
+    sum_of_primes = 0
+    for x in range(2,2000000):
+        if isprime(x):
+            sum_of_primes += x
+    return sum_of_primes
+
+
+print(summation_of_primes())
