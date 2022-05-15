6dda12b3e644b226dd7c82e9f2ff05b13e8a2404
Gioack <101208747+Gioack@users.noreply.github.com>
2022-05-15 11:26:14 +0200

    problem solved

---
+++ P24_lexicographic_permutations.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+
+def lexicographic_permutations():
+    for count,permutation in enumerate(permutations("0123456789")):
+        if count == 999999:
+            return "".join(permutation)
+                 
+
+print(lexicographic_permutations())
