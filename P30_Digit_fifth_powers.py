c369cd01b7e23d21a2eed0b0c240b686268a3d54
Gioack <101208747+Gioack@users.noreply.github.com>
2022-05-18 15:21:13 +0200

    problem solved

---
+++ P30_Digit_fifth_powers.py
@@ -0,0 +1,17 @@
+def digit_fifth_powers():
+    sum_of_numbers_that_can_be_written_as_the_sum_of_fifth_powers_of_their_digits = 0
+    for number in range(2,1000000):
+        sum_of_fith_powers_of_digits = get_sum_of_the_fifth_powers_of_each_digits(number)
+        if sum_of_fith_powers_of_digits == number:
+            sum_of_numbers_that_can_be_written_as_the_sum_of_fifth_powers_of_their_digits += sum_of_fith_powers_of_digits
+    return sum_of_numbers_that_can_be_written_as_the_sum_of_fifth_powers_of_their_digits
+
+
+def get_sum_of_the_fifth_powers_of_each_digits(number):
+    sum_of_fith_power_of_digits = 0
+    for digit in str(number):
+        sum_of_fith_power_of_digits += int(digit)**5
+    return sum_of_fith_power_of_digits
+
+
+print(digit_fifth_powers())
