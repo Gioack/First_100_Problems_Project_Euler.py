f74781619b0800337ef6939e251074d7f9613e02
Gioack <101208747+Gioack@users.noreply.github.com>
2022-05-30 11:07:48 +0200

    cleaned

--- P 3_Prime_Factors.py
+++ P 3_Prime_Factors.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def is_prime(num):
     for n in range(2, int(num**0.5)+1):
         if num % n == 0:
@@ -14,14 +17,19 @@ def get_smallest_prime_divisor(number):
     return divisor_prime
 
 
-def largest_prime_factor(number):
-    largest_divisor = 1
+def get_prime_factors(number):
+    prime_factors = list()
     while number != 1:
         smallest_prime_divisor = get_smallest_prime_divisor(number)
         number /= smallest_prime_divisor
-        if smallest_prime_divisor > largest_divisor:
-            largest_divisor = smallest_prime_divisor
-    return largest_divisor
+        prime_factors.append(smallest_prime_divisor)
+    return Counter(prime_factors)
 
 
-print(largest_prime_factor(600851475143))
+def largest_prime_factor():
+    number = 600851475143
+    all_factors = get_prime_factors(number).keys()
+    return max(all_factors)
+
+
+print(largest_prime_factor())
