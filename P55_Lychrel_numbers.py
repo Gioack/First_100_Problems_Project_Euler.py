6046340750daf86adfd940b0d29974432dfe396d
Gioack <101208747+Gioack@users.noreply.github.com>
2022-06-05 14:01:22 +0200

    problem solved

---
+++ P55_Lychrel_numbers.py
@@ -0,0 +1,26 @@
+def solution():
+    lychrel_numbers = 0
+    for x in range(10000+1):
+        if is_lychrel(x):
+            lychrel_numbers += 1
+    return lychrel_numbers
+
+
+def is_lychrel(number):
+    sum_numbers = number
+    for _ in range(50):
+        sum_numbers += get_inverse(sum_numbers)
+        if is_palindromic(sum_numbers):
+            return False
+    return True
+
+
+def get_inverse(number):
+    return int(str(number)[::-1])
+
+
+def is_palindromic(number):
+    return get_inverse(number) == number
+
+
+print(solution())
