65cbb5243a6e3e409268401c13645d9c435e5f1d
Gioack <101208747+Gioack@users.noreply.github.com>
2022-06-10 10:14:32 +0200

    made code cleaner

--- P63_Powerful_digit_counts.py
+++ P63_Powerful_digit_counts.py
@@ -1,24 +1,16 @@
 def powerful_digit_counts():
-    number_of_this_kind = 0
-    for x in range(1, 1000):
-        counter_1 = 0
-        counter_2 = 0
-        i = 0
-        power = x**i
+    counter_number_of_this_kind = 0
+    for n in range(1, 10):
+        exponent = 1
+        power_of_n = n**exponent
         while True:
-            if len(str(power)) <= i:
-                counter_1 += 1
-                counter_2 = 0
-            if len(str(power)) >= i:
-                counter_2 += 1
-                counter_1 = 0
-            if (counter_1 > 2) or (counter_2 > 2):
+            if len(str(power_of_n)) == exponent:
+                counter_number_of_this_kind += 1
+            else:
                 break
-            if len(str(power)) == i:
-                number_of_this_kind += 1
-            i += 1
-            power = x**i
-    return number_of_this_kind
+            exponent += 1
+            power_of_n = n**exponent
+    return counter_number_of_this_kind
 
 
 print(powerful_digit_counts())
