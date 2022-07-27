682f539d9b88f5941a8cbc354e1e18be38286aa0
Gioack <101208747+Gioack@users.noreply.github.com>
2022-07-27 11:48:59 +0200

    small change

--- P 2_Even_Fibonacci_numbers.py
+++ P 2_Even_Fibonacci_numbers.py
@@ -5,7 +5,8 @@ def is_even(num):
 def even_fibonacci_numbers():
     fibonacci_numbers = [0, 1]
     while fibonacci_numbers[-1] < 4000000:
-        fibonacci_numbers.append(fibonacci_numbers[-2]+fibonacci_numbers[-1])
+        fibonacci_numbers.append(
+            fibonacci_numbers[-2]+fibonacci_numbers[-1])
     return sum(filter(is_even, fibonacci_numbers))
 
 
