fd8324b9069bedf04f520cced1f9ce7538d0f9a6
Gioack <101208747+Gioack@users.noreply.github.com>
2022-07-28 21:48:18 +0200

    refactored 36

--- problems_from_31_to_40/P36_Double_base_palindromes.py
+++ problems_from_31_to_40/P36_Double_base_palindromes.py
@@ -1,12 +1,23 @@
 def double_base_palindromes():
     sum_of_all_double_base_palindromes = 0
     for number in range(1000000):
-        palindrom_base_10 = str(number)[::-1]
-        number_base_2 = str(bin(number)[2:])
-        palindrom_base_2 = number_base_2[::-1]
-        if (palindrom_base_10 == str(number)) and (palindrom_base_2 == number_base_2):
+        if is_double_base_palindrome(number):
             sum_of_all_double_base_palindromes += number
     return sum_of_all_double_base_palindromes
+
+
+def is_double_base_palindrome(number):
+    number_in_base_2 = to_base_2(number)
+    return is_palindrome(number) and is_palindrome(number_in_base_2)
+
+
+def to_base_2(number):
+    return bin(number)[2:]
+
+
+def is_palindrome(number):
+    palindrome_base_10 = str(number)[::-1]
+    return (palindrome_base_10 == number)
 
 
 print(double_base_palindromes())
