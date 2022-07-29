8698e58bc01f057b201b229945f5766a1bb4cb1d
Gioack <101208747+Gioack@users.noreply.github.com>
2022-07-29 20:37:13 +0200

    refactored P17

--- problems_from_11_to_20/P17_Number_letter_counts.py
+++ problems_from_11_to_20/P17_Number_letter_counts.py
@@ -1,35 +1,43 @@
-def count_letter(num):
-    number_of_Letter = 0
-    num = str(num)
-    units_letter_count = {"1": 3, "2": 3, "3": 5, "4": 4,
-                          "5": 4, "6": 3, "7": 5, "8": 5, "9": 4, "0": 0}
+def count_letter(number):
+    number = str(number)
+    if len(number) == 1:
+        return count_letter_1_digits(number)
+    elif len(number) == 2:
+        return count_letter_2_digits(number)
+    elif len(number) == 3:
+        return count_letter_3_digits(number)
+
+
+def count_letter_1_digits(number):
+    unit_letter_count = {"1": 3, "2": 3, "3": 5, "4": 4,
+                         "5": 4, "6": 3, "7": 5, "8": 5, "9": 4, "0": 0}
+    return unit_letter_count[number]
+
+
+def count_letter_2_digits(number):
+    tens = number[0]
+    unit = number[1]
+    exceptions_count = {
+        "10": 3, "11": 6, "12": 6, "13": 8,
+        "14": 8, "15": 7, "16": 7, "17": 9, "18": 8, "19": 8}
     tens_letter_count = {"1": 3, "2": 6, "3": 6, "4": 5,
                          "5": 5, "6": 5, "7": 7, "8": 6, "9": 6, "0": 0}
-    count_exceptions = {"10": 3, "11": 6, "12": 6, "13": 8,
-                        "14": 8, "15": 7, "16": 7, "17": 9, "18": 8, "19": 8}
+    if 10 < int(number) < 20:
+        return exceptions_count[number]
+    else:
+        return tens_letter_count[tens]+count_letter_1_digits(unit)
 
-    if len(num) == 1:
-        number_of_Letter += units_letter_count[num]
 
-    if len(num) == 2:
-        if num[0] != "1":
-            number_of_Letter += units_letter_count[num[1]]
-            number_of_Letter += tens_letter_count[num[0]]
-        else:
-            number_of_Letter += count_exceptions[num]
-
-    if len(num) == 3:
-        if (num[2] != "0") or (num[1] != "0"):
-            number_of_Letter += units_letter_count[num[0]]+10
-        else:
-            number_of_Letter += units_letter_count[num[0]]+7
-        if num[1] != "1":
-            number_of_Letter += tens_letter_count[num[1]]
-            number_of_Letter += units_letter_count[num[2]]
-        else:
-            number_of_Letter += count_exceptions[num[1:]]
-
-    return number_of_Letter
+def count_letter_3_digits(number):
+    hundreds = number[0]
+    tens_unit = number[1:]
+    count_tens_and_unit = count_letter_2_digits(tens_unit)
+    count_hundreds = count_letter_1_digits(
+        hundreds)+len("hundred")
+    count_letters = count_hundreds+count_tens_and_unit
+    if count_tens_and_unit != 0:
+        count_letters += len("and")
+    return count_letters
 
 
 def number_letter_counts():
@@ -39,4 +47,5 @@ def number_letter_counts():
     return counter
 
 
+print(count_letter(21))
 print(number_letter_counts())
