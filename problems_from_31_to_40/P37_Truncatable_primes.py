72e46f1494d3540382ec802f9278933e53fe95b9
Gioack <101208747+Gioack@users.noreply.github.com>
2022-07-29 20:37:32 +0200

    corrected typos

--- problems_from_31_to_40/P37_Truncatable_primes.py
+++ problems_from_31_to_40/P37_Truncatable_primes.py
@@ -2,26 +2,26 @@ from sympy import primerange, isprime
 
 
 class truncatable_primes():
-    def get_sum_of_truncable_primes(self):
-        sum_truncable_primes = list()
+    def get_sum_of_truncatable_primes(self):
+        sum_truncatable_primes = list()
         for prime in primerange(1000000):
             if prime < 10:
                 continue
-            if self.is_truncable(prime):
-                sum_truncable_primes.append(prime)
-        return sum(sum_truncable_primes)
+            if self.is_truncatable(prime):
+                sum_truncatable_primes.append(prime)
+        return sum(sum_truncatable_primes)
 
-    def is_truncable(self, number):
-        return (self.is_truncable_from_left(number)) and (self.is_truncable_from_right(number))
+    def is_truncatable(self, number):
+        return (self.is_truncatable_from_left(number)) and (self.is_truncatable_from_right(number))
 
-    def is_truncable_from_left(self, number):
+    def is_truncatable_from_left(self, number):
         while number != "":
             if isprime(int(number)) == False:
                 return False
             number = str(number)[1:]
         return True
 
-    def is_truncable_from_right(self, number):
+    def is_truncatable_from_right(self, number):
         while number != "":
             if isprime(int(number)) == False:
                 return False
@@ -29,4 +29,4 @@ class truncatable_primes():
         return True
 
 
-print(truncatable_primes().get_sum_of_truncable_primes())
+print(truncatable_primes().get_sum_of_truncatable_primes())
